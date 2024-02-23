05a33df1e1786ecab16e21a9f85122fde6139872
Amit Vishwakarma <amit1999vishwakarma@gmail.com>
2024-02-23 12:51:03 +0530

    Update program1.js

--- program1.js
+++ program1.js
@@ -3,9 +3,20 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    
+    let stack = [];
+    let mapping = {")": "(", "}": "{", "]": "["};
+
+    for (let char of s) {
+        if (char in mapping) {
+            let topElement = stack.length === 0 ? '#' : stack.pop();
+            if (mapping[char] !== topElement) {
+                return false;
+            }
+        } else {
+            stack.push(char);
+        }
+    }
+    return stack.length === 0;
 };
 
 module.exports = { isValid };
-
-
