695692711c08661eb94fbb7f8396d3684a034fbd
Amit Vishwakarma <amit1999vishwakarma@gmail.com>
2024-02-23 12:55:08 +0530

    Update program2.js

--- program2.js
+++ program2.js
@@ -3,8 +3,25 @@
  * @return {number}
  */
 var romanToInt = function(s) {
-    
+    let values = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
+
+    let total = 0;
+    for (let i = 0; i < s.length; i++) {
+        if (values[s[i]] < values[s[i + 1]]) {
+            total -= values[s[i]];
+        } else {
+            total += values[s[i]];
+        }
+    }
+    return total;
 };
 
-
-module.exports={romanToInt}
+module.exports = { romanToInt };
